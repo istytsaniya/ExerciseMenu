05130d745d29519f59b9fdb18f2719bbc9a8aa64
istytsaniya <tsaniyaisty08@gmail.com>
2020-04-07 11:56:24 +0700

    add keliling lingkaran

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.exercisemenu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button btnok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.editUN);
+        password = (EditText) findViewById(R.id.editPass);
+        btnok = (Button) findViewById(R.id.btnok);
+
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usernameKey = username.getText().toString();
+                String passwordKey = password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                    //jika login sukses
+                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES LURRRRRR",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, activity_2.class);
+                    MainActivity.this.startActivity(intent);
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password anda salah lurrr !")
+                            .setNegativeButton("Retry",null).create().show();
+                }
+            }
+        });
     }
 }
